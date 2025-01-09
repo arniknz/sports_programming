99cbe7e8af4dec11ed3c2c6f6d99877920df2e46
Arn K <arniknz@gmail.com>
2025-01-09 21:05:21 +0300

    t_bank_kruzhok_otbor/A.cpp

--- t_bank_kruzhok_otbor/A.py
+++ t_bank_kruzhok_otbor/A.py
@@ -1,1 +1,14 @@
+# I
+a = int(input())
 
+if a == 1:
+    print(1)
+    exit()
+
+
+n1 = 1 % a
+for n in range(1, 10**12):
+    n1 = (n * n) % a
+    if n1 % a == 0:
+        print(n)
+        break
